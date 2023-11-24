eebabf28c847bfa2ebf98e66f0329637cea78b98
akmlrnyn <akmalranyan@gmail.com>
2023-11-24 08:59:08 +0700

    made routes and views, initialize eslint

--- utils/contacts.js
+++ utils/contacts.js
@@ -1,28 +1,28 @@
 const fs = require('fs');
 const readline = require('readline');
 
-//membuat direktori folder jika belum ada
-const dirPath = './data'
+// membuat direktori folder jika belum ada
+const dirPath = './data';
 if (!fs.existsSync(dirPath)) {
-fs.mkdirSync(dirPath);
+  fs.mkdirSync(dirPath);
 }
 
-//membuat file jika belum ada
-const dataPath = './data/contacts.json'
+// membuat file jika belum ada
+const dataPath = './data/contacts.json';
 if (!fs.existsSync(dataPath)) {
-fs.writeFileSync(dataPath, '[]', 'utf-8');
+  fs.writeFileSync(dataPath, '[]', 'utf-8');
 }
 
 const loadContact = () => {
-    const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
-    const contacts = JSON.parse(fileBuffer);
-    return contacts;
-}
+  const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
+  const contacts = JSON.parse(fileBuffer);
+  return contacts;
+};
 
 const findContact = (nama) => {
-    const contacts = loadContact();
-    const result = contacts.find((contact) => contact.nama.toLowerCase() === nama.toLowerCase());
-    return result
-}
+  const contacts = loadContact();
+  const result = contacts.find((contact) => contact.nama.toLowerCase() === nama.toLowerCase());
+  return result;
+};
 
-module.exports = { loadContact, findContact }
+module.exports = { loadContact, findContact };
